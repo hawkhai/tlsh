280322f06a9c7d92cc1e1a3388d33e9252ef8b09
haihoing <haihoing@qq.com>
2021-09-04 00:05:47 +0800

    tlsh_module.h

--- include/tlsh_module.h
+++ include/tlsh_module.h
@@ -7,36 +7,40 @@
 // see 4.0.0 from 26/Mar/2020 at https://github.com/trendmicro/tlsh/blob/master/Change_History.md
 #define SHOWVERSION	1
 
-static std::string eval_tlsh(unsigned char* pBuffer, unsigned int len, int showvers);
-static std::string hash_py(unsigned char* pBuffer, unsigned int len);
-static std::string oldhash_py(unsigned char* pBuffer, unsigned int len);
-static std::string forcehash_py(unsigned char* pBuffer, unsigned int len);
-static std::string eval_cons_tlsh(unsigned char* pBuffer, unsigned int len, int showvers);
-static std::string conservativehash_py(unsigned char* pBuffer, unsigned int len);
-static std::string oldconservativehash_py(unsigned char* pBuffer, unsigned int len);
-
-static int diff_py(char* hash1, char* hash2);
-static int diffxlen_py(char* hash1, char* hash2);
-
 typedef struct {
     unsigned int required_data; // = 0;
     bool finalized; // = false;
     Tlsh tlsh;
 } tlsh_TlshObject;
 
-static tlsh_TlshObject* Tlsh_create(const char* str, unsigned int len);
-static tlsh_TlshObject* Tlsh_fromTlshStr(const char* str, unsigned int len);
-static void Tlsh_release(tlsh_TlshObject* self);
+namespace tlsh {
 
-void Tlsh_update(tlsh_TlshObject* self, const char* str, unsigned int len);
-void Tlsh_final(tlsh_TlshObject* self);
-std::string Tlsh_hexdigest(tlsh_TlshObject* self);
+    std::string eval_tlsh(unsigned char* pBuffer, unsigned int len, int showvers);
+    std::string hash_py(unsigned char* pBuffer, unsigned int len);
+    std::string oldhash_py(unsigned char* pBuffer, unsigned int len);
+    std::string forcehash_py(unsigned char* pBuffer, unsigned int len);
+    std::string eval_cons_tlsh(unsigned char* pBuffer, unsigned int len, int showvers);
+    std::string conservativehash_py(unsigned char* pBuffer, unsigned int len);
+    std::string oldconservativehash_py(unsigned char* pBuffer, unsigned int len);
 
-int Tlsh_diff(tlsh_TlshObject* self, char* str, unsigned int len);
-int Tlsh_diff(tlsh_TlshObject* self, tlsh_TlshObject* other_tlsh);
-int Tlsh_lvalue(tlsh_TlshObject* self);
-int Tlsh_q1ratio(tlsh_TlshObject* self);
-int Tlsh_q2ratio(tlsh_TlshObject* self);
-bool Tlsh_is_valid(tlsh_TlshObject* self);
-int Tlsh_checksum(tlsh_TlshObject* self, int id);
-int Tlsh_bucket_value(tlsh_TlshObject* self, int id);
+    int diff_py(const char* hash1, const char* hash2);
+    int diffxlen_py(const char* hash1, const char* hash2);
+
+    tlsh_TlshObject* Tlsh_create(const char* str, unsigned int len);
+    tlsh_TlshObject* Tlsh_fromTlshStr(const char* str, unsigned int len);
+    void Tlsh_release(tlsh_TlshObject* self);
+
+    void Tlsh_update(tlsh_TlshObject* self, const char* str, unsigned int len);
+    void Tlsh_final(tlsh_TlshObject* self);
+    std::string Tlsh_hexdigest(tlsh_TlshObject* self);
+
+    int Tlsh_diff(tlsh_TlshObject* self, char* str, unsigned int len);
+    int Tlsh_diff(tlsh_TlshObject* self, tlsh_TlshObject* other_tlsh);
+    int Tlsh_lvalue(tlsh_TlshObject* self);
+    int Tlsh_q1ratio(tlsh_TlshObject* self);
+    int Tlsh_q2ratio(tlsh_TlshObject* self);
+    bool Tlsh_is_valid(tlsh_TlshObject* self);
+    int Tlsh_checksum(tlsh_TlshObject* self, int id);
+    int Tlsh_bucket_value(tlsh_TlshObject* self, int id);
+
+}
--- src/tlsh_module.cpp
+++ src/tlsh_module.cpp
@@ -1,225 +1,229 @@
 #include "tlsh.h"
 #include "tlsh_module.h"
 
-static std::string eval_tlsh(unsigned char* pBuffer, unsigned int len, int showvers)
-{
-    Tlsh tlsh;
-    tlsh.update(pBuffer, len);
-    tlsh.final();
-    const char* s = tlsh.getHash(showvers);
-    if (*s == '\0')
-        return "TNULL";
-    return s;
-}
+namespace tlsh {
 
-static std::string hash_py(unsigned char* pBuffer, unsigned int len)
-{
-    return (eval_tlsh(pBuffer, len, SHOWVERSION));
-}
-
-static std::string oldhash_py(unsigned char* pBuffer, unsigned int len) {
-    return (eval_tlsh(pBuffer, len, 0));
-}
-
-static std::string forcehash_py(unsigned char* pBuffer, unsigned int len)
-{
-    return (hash_py(pBuffer, len));
-}
-
-static std::string eval_cons_tlsh(unsigned char* pBuffer, unsigned int len, int showvers)
-{
-    Tlsh tlsh;
-    tlsh.update(pBuffer, len);
-    tlsh.final(NULL, 0, 2);
-    const char* s = tlsh.getHash(showvers);
-    if (*s == '\0')
-        return "TNULL";
-    return s;
-}
-
-static std::string conservativehash_py(unsigned char* pBuffer, unsigned int len)
-{
-    return (eval_cons_tlsh(pBuffer, len, SHOWVERSION));
-}
-
-static std::string oldconservativehash_py(unsigned char* pBuffer, unsigned int len)
-{
-    return (eval_cons_tlsh(pBuffer, len, 0));
-}
-
-static int diff_py(char* hash1, char* hash2) {
-    Tlsh tlsh1, tlsh2;
-    if (tlsh1.fromTlshStr(hash1) != 0) {
-        throw "argument %s is not a TLSH hex string";
-    }
-    if (tlsh2.fromTlshStr(hash2) != 0) {
-        throw "argument %s is not a TLSH hex string";
+    std::string eval_tlsh(unsigned char* pBuffer, unsigned int len, int showvers)
+    {
+        Tlsh tlsh;
+        tlsh.update(pBuffer, len);
+        tlsh.final();
+        const char* s = tlsh.getHash(showvers);
+        if (*s == '\0')
+            return "TNULL";
+        return s;
     }
 
-    int score = tlsh1.totalDiff(&tlsh2);
-    return score;
-}
-
-static int diffxlen_py(char* hash1, char* hash2) {
-    Tlsh tlsh1, tlsh2;
-    if (tlsh1.fromTlshStr(hash1) != 0) {
-        throw "argument %s is not a TLSH hex string";
-    }
-    if (tlsh2.fromTlshStr(hash2) != 0) {
-        throw "argument %s is not a TLSH hex string";
-    }
-    int score = tlsh1.totalDiff(&tlsh2, false);
-    return score;
-}
-
-static tlsh_TlshObject* Tlsh_create(const char* str, unsigned int len)
-{
-    tlsh_TlshObject* tlsh_object = new tlsh_TlshObject();
-
-    /* Call Tlsh() constructor. */
-    // new (&tlsh_object->tlsh) Tlsh();
-    tlsh_object->required_data = 0;
-    tlsh_object->finalized = false;
-
-    if (str && len) {
-        Tlsh_update(tlsh_object, str, len);
-    }
-    return 0;
-}
-
-static void Tlsh_release(tlsh_TlshObject* self) {
-    delete self;
-}
-
-static tlsh_TlshObject* Tlsh_fromTlshStr(const char* str, unsigned int len)
-{
-    if ((len != TLSH_STRING_LEN_REQ) && (len != TLSH_STRING_LEN_REQ - 2)) {
-        throw "argument length incorrect: not a TLSH hex string";
-        return NULL;
+    std::string hash_py(unsigned char* pBuffer, unsigned int len)
+    {
+        return (eval_tlsh(pBuffer, len, SHOWVERSION));
     }
 
-    tlsh_TlshObject* self = Tlsh_create(str, len);
+    std::string oldhash_py(unsigned char* pBuffer, unsigned int len) {
+        return (eval_tlsh(pBuffer, len, 0));
+    }
 
-    if (self->tlsh.fromTlshStr(str) != 0) {
+    std::string forcehash_py(unsigned char* pBuffer, unsigned int len)
+    {
+        return (hash_py(pBuffer, len));
+    }
+
+    std::string eval_cons_tlsh(unsigned char* pBuffer, unsigned int len, int showvers)
+    {
+        Tlsh tlsh;
+        tlsh.update(pBuffer, len);
+        tlsh.final(NULL, 0, 2);
+        const char* s = tlsh.getHash(showvers);
+        if (*s == '\0')
+            return "TNULL";
+        return s;
+    }
+
+    std::string conservativehash_py(unsigned char* pBuffer, unsigned int len)
+    {
+        return (eval_cons_tlsh(pBuffer, len, SHOWVERSION));
+    }
+
+    std::string oldconservativehash_py(unsigned char* pBuffer, unsigned int len)
+    {
+        return (eval_cons_tlsh(pBuffer, len, 0));
+    }
+
+    int diff_py(const char* hash1, const char* hash2) {
+        Tlsh tlsh1, tlsh2;
+        if (tlsh1.fromTlshStr(hash1) != 0) {
+            throw "argument %s is not a TLSH hex string";
+        }
+        if (tlsh2.fromTlshStr(hash2) != 0) {
+            throw "argument %s is not a TLSH hex string";
+        }
+
+        int score = tlsh1.totalDiff(&tlsh2);
+        return score;
+    }
+
+    int diffxlen_py(const char* hash1, const char* hash2) {
+        Tlsh tlsh1, tlsh2;
+        if (tlsh1.fromTlshStr(hash1) != 0) {
+            throw "argument %s is not a TLSH hex string";
+        }
+        if (tlsh2.fromTlshStr(hash2) != 0) {
+            throw "argument %s is not a TLSH hex string";
+        }
+        int score = tlsh1.totalDiff(&tlsh2, false);
+        return score;
+    }
+
+    tlsh_TlshObject* Tlsh_create(const char* str, unsigned int len)
+    {
+        tlsh_TlshObject* tlsh_object = new tlsh_TlshObject();
+
+        /* Call Tlsh() constructor. */
+        // new (&tlsh_object->tlsh) Tlsh();
+        tlsh_object->required_data = 0;
+        tlsh_object->finalized = false;
+
+        if (str && len) {
+            Tlsh_update(tlsh_object, str, len);
+        }
+        return tlsh_object;
+    }
+
+    void Tlsh_release(tlsh_TlshObject* self) {
         delete self;
-        throw "argument value incorrect: not a TLSH hex string";
-        return NULL;
     }
-    self->finalized = true;
-    return self;
-}
 
-void Tlsh_update(tlsh_TlshObject* self, const char* str, unsigned int len)
-{
-    if (self->finalized) {
-        throw "final() has already been called";
-        return;
-    }
-    if (self->required_data < MIN_DATA_LENGTH) {
-        self->required_data += len > MIN_DATA_LENGTH ? MIN_DATA_LENGTH : len;
-    }
-    self->tlsh.update((unsigned char*)str, (unsigned int)len);
-}
+    tlsh_TlshObject* Tlsh_fromTlshStr(const char* str, unsigned int len)
+    {
+        if ((len != TLSH_STRING_LEN_REQ) && (len != TLSH_STRING_LEN_REQ - 2)) {
+            throw "argument length incorrect: not a TLSH hex string";
+            return NULL;
+        }
 
-void Tlsh_final(tlsh_TlshObject* self)
-{
-    if (self->finalized) {
-        throw "final() has already been called";
-        return;
-    }
-    if (self->required_data < MIN_DATA_LENGTH) {
-        throw "less than %u of input";
-        return;
-    }
-    self->finalized = true;
-    self->tlsh.final();
-}
+        tlsh_TlshObject* self = Tlsh_create(str, len);
 
-std::string Tlsh_hexdigest(tlsh_TlshObject* self)
-{
-    char hash[TLSH_STRING_LEN_REQ + 1];
-    if (!self->finalized) {
-        throw "final() has not been called";
-        return NULL;
+        if (self->tlsh.fromTlshStr(str) != 0) {
+            delete self;
+            throw "argument value incorrect: not a TLSH hex string";
+            return NULL;
+        }
+        self->finalized = true;
+        return self;
     }
-    self->tlsh.getHash(hash, TLSH_STRING_LEN_REQ + 1, SHOWVERSION);
-    if (hash[0] == '\0') {
-        throw "error while getting hash (not enough variation in input?)";
-        return NULL;
+
+    void Tlsh_update(tlsh_TlshObject* self, const char* str, unsigned int len)
+    {
+        if (self->finalized) {
+            throw "final() has already been called";
+            return;
+        }
+        if (self->required_data < MIN_DATA_LENGTH) {
+            self->required_data += len > MIN_DATA_LENGTH ? MIN_DATA_LENGTH : len;
+        }
+        self->tlsh.update((unsigned char*)str, (unsigned int)len);
     }
-    return hash;
-}
 
-int Tlsh_diff(tlsh_TlshObject* self, char* str, unsigned int len)
-{
-    int score;
-
-    Tlsh other;
-    if ((len != TLSH_STRING_LEN_REQ) && (len != TLSH_STRING_LEN_REQ - 2)) {
-        throw "argument is not a TLSH hex string";
-        return NULL;
+    void Tlsh_final(tlsh_TlshObject* self)
+    {
+        if (self->finalized) {
+            throw "final() has already been called";
+            return;
+        }
+        if (self->required_data < MIN_DATA_LENGTH) {
+            throw "less than %u of input";
+            return;
+        }
+        self->finalized = true;
+        self->tlsh.final();
     }
-    if (other.fromTlshStr(str) != 0) {
-        throw "argument is not a TLSH hex string";
-        return NULL;
+
+    std::string Tlsh_hexdigest(tlsh_TlshObject* self)
+    {
+        char hash[TLSH_STRING_LEN_REQ + 1];
+        if (!self->finalized) {
+            throw "final() has not been called";
+            return NULL;
+        }
+        self->tlsh.getHash(hash, TLSH_STRING_LEN_REQ + 1, SHOWVERSION);
+        if (hash[0] == '\0') {
+            throw "error while getting hash (not enough variation in input?)";
+            return NULL;
+        }
+        return hash;
     }
-    score = self->tlsh.totalDiff(&other);
-    return score;
-}
 
-int Tlsh_diff(tlsh_TlshObject* self, tlsh_TlshObject* other_tlsh)
-{
-    int score;
-    score = self->tlsh.totalDiff(&other_tlsh->tlsh);
-    return score;
-}
+    int Tlsh_diff(tlsh_TlshObject* self, char* str, unsigned int len)
+    {
+        int score;
 
-int Tlsh_lvalue(tlsh_TlshObject* self)
-{
-    if (!self->finalized) {
-        throw "final() has not been called";
-        return NULL;
+        Tlsh other;
+        if ((len != TLSH_STRING_LEN_REQ) && (len != TLSH_STRING_LEN_REQ - 2)) {
+            throw "argument is not a TLSH hex string";
+            return NULL;
+        }
+        if (other.fromTlshStr(str) != 0) {
+            throw "argument is not a TLSH hex string";
+            return NULL;
+        }
+        score = self->tlsh.totalDiff(&other);
+        return score;
     }
-    return self->tlsh.Lvalue();
-}
 
-int Tlsh_q1ratio(tlsh_TlshObject* self)
-{
-    if (!self->finalized) {
-        throw "final() has not been called";
-        return NULL;
+    int Tlsh_diff(tlsh_TlshObject* self, tlsh_TlshObject* other_tlsh)
+    {
+        int score;
+        score = self->tlsh.totalDiff(&other_tlsh->tlsh);
+        return score;
     }
-    return self->tlsh.Q1ratio();
-}
 
-int Tlsh_q2ratio(tlsh_TlshObject* self)
-{
-    if (!self->finalized) {
-        throw "final() has not been called";
-        return NULL;
+    int Tlsh_lvalue(tlsh_TlshObject* self)
+    {
+        if (!self->finalized) {
+            throw "final() has not been called";
+            return NULL;
+        }
+        return self->tlsh.Lvalue();
     }
-    return self->tlsh.Q2ratio();
-}
 
-bool Tlsh_is_valid(tlsh_TlshObject* self)
-{
-    return self->tlsh.isValid();
-}
-
-int Tlsh_checksum(tlsh_TlshObject* self, int id)
-{
-    if (!self->finalized) {
-        throw "final() has not been called";
-        return NULL;
+    int Tlsh_q1ratio(tlsh_TlshObject* self)
+    {
+        if (!self->finalized) {
+            throw "final() has not been called";
+            return NULL;
+        }
+        return self->tlsh.Q1ratio();
     }
-    return self->tlsh.Checksum(id);
-}
 
-int Tlsh_bucket_value(tlsh_TlshObject* self, int id)
-{
-    if (!self->finalized) {
-        throw "final() has not been called";
-        return NULL;
+    int Tlsh_q2ratio(tlsh_TlshObject* self)
+    {
+        if (!self->finalized) {
+            throw "final() has not been called";
+            return NULL;
+        }
+        return self->tlsh.Q2ratio();
     }
-    return self->tlsh.BucketValue(id);
+
+    bool Tlsh_is_valid(tlsh_TlshObject* self)
+    {
+        return self->tlsh.isValid();
+    }
+
+    int Tlsh_checksum(tlsh_TlshObject* self, int id)
+    {
+        if (!self->finalized) {
+            throw "final() has not been called";
+            return NULL;
+        }
+        return self->tlsh.Checksum(id);
+    }
+
+    int Tlsh_bucket_value(tlsh_TlshObject* self, int id)
+    {
+        if (!self->finalized) {
+            throw "final() has not been called";
+            return NULL;
+        }
+        return self->tlsh.BucketValue(id);
+    }
+
 }
